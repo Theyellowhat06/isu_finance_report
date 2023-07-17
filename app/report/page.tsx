3e16b6796af6615446fd07025c3bddf790e4e565
Theyellowhat06 <42085659+Theyellowhat06@users.noreply.github.com>
2023-07-17 09:23:52 +0800

    first

---
+++ app/report/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Button, Input } from "@material-tailwind/react";
+
+export default function Report() {
+  return (
+    <div>
+      <Button>button</Button>
+      <Input type={"file"}></Input>
+    </div>
+  );
+}
